1f3b73fdf24985eef6f8bbb810ecaa92ccd009e5
root <yerekimeatonbaa@gmail.com>
2023-11-24 16:40:05 -0800

    first commit

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+#creating a file using pupet
+
+file   { 'school':
+ensure    => 'present',
+content   => 'I love Puppet',
+group     =>  'www-data',
+mode      =>  '0744',
+owner     =>  'www-data',
+path      =>  '/tmp/school',
+}
